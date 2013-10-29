5bdc353cd0291f6c3e9ee82605995d792fd59a5c
yukinami <yuki-nami@hotmail.com>
2013-10-29 13:18:03 +0800

    first created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AnchoringMng::Application.config.secret_token = 'fefb146852bddbd69f1fd3a95a013814456ecd4071758abbd8a02b507902aff9c0c2d0417d0b953cdadcef41569706be965c16a954f2e70293901d91f33a8afe'
